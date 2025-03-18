39c718b18ae09c4d34b231f4de8414dfce0b2c08
Fabioguima <37258543+Fabioguima@users.noreply.github.com>
2025-03-17 22:14:20 -0300

    Resolução aula 2.1

--- 02_Funcoes_e_TDD/converte_horas.py
+++ 02_Funcoes_e_TDD/converte_horas.py
@@ -7,7 +7,11 @@ def minuto_para_segundo(valor):
 
 def hora_para_segundo(valor):
     return valor * 60 * 60
-    #outra forma de resolver (resolve dos () para fora) ---> minuto_para_segundohora_para_minuto(valor)
+    #outra forma de resolver (resolve dos () para fora) --->       minuto_para_segundohora_para_minuto(valor)
+
+def dias_para_segundo(valor):
+    #return valor 24 * 60 * 60
+    return hora_para_minuto (minuto_para_segundo(valor)) * 24
 
 assert hora_para_minuto(0) == 0
 assert hora_para_minuto(1) == 60
@@ -20,5 +24,8 @@ assert minuto_para_segundo(2) == 120
 assert hora_para_segundo(0) == 0
 assert hora_para_segundo(1) == 3600  # 1 * 60 * 60
 assert hora_para_segundo(2) == 7200  # 2 * 60 * 60
+
+assert dia_para_segundo(1) == 86400
+assert dia_para_segundo(2) == 172800
 
 print("Terminou com sucesso!")
