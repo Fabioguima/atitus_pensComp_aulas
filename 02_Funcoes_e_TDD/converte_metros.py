275ead66fa95642d9e525cdcb78c8b5470793f2f
Fabioguima <37258543+Fabioguima@users.noreply.github.com>
2025-03-17 22:28:32 -0300

    vereficar

--- 02_Funcoes_e_TDD/converte_metros.py
+++ 02_Funcoes_e_TDD/converte_metros.py
@@ -6,7 +6,7 @@ def centimetro_para_metro(valor):
   return valor / 100
   
 
-  
+
 assert metro_para_centimetro(1) == 100
 assert metro_para_centimetro(10) == 1000
 assert metro_para_centimetro(0) == 0
@@ -16,3 +16,5 @@ assert centimetro_para_metro(1000) == 10
 assert centimetro_para_metro(0) == 0
 
 print("Terminou com sucesso!")
+
+#alo
