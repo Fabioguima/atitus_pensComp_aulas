a9c56c7838c47b86982573b10f25ce9732af891e
Fábio Guimarães <37258543+Fabioguima@users.noreply.github.com>
2025-03-19 22:18:33 -0300

    def test 2

--- 03_variaveis_e_operadores_matematicos/pitagoras.py
+++ 03_variaveis_e_operadores_matematicos/pitagoras.py
@@ -10,18 +10,17 @@ def hipotenusa(largura, altura):
     return (((largura ** 2) + (altura ** 2)) ** (1/2))
 
 def test():
-    
-assert area(5, 5) == 25
-assert area(0, 5) == 0
-assert area(10, 5) == 50
+    assert area(5, 5) == 25
+    assert area(0, 5) == 0
+    assert area(10, 5) == 50
 
-assert perimetro(2, 3) == 10
-assert perimetro(5, 5) == 20
-assert perimetro(10, 5) == 30
+    assert perimetro(2, 3) == 10
+    assert perimetro(5, 5) == 20
+    assert perimetro(10, 5) == 30
 
-assert hipotenusa(4, 3) == 5
-assert hipotenusa(6, 8) == 10
-assert hipotenusa(5, 12) == 13
+    assert hipotenusa(4, 3) == 5
+    assert hipotenusa(6, 8) == 10
+    assert hipotenusa(5, 12) == 13
 
 print('Hello world')
 print(bool(hipotenusa(4, 3) == 5))
