45fa1c105039a0dc69c8dccb61b84aa43e9ee161
Fábio Guimarães <37258543+Fabioguima@users.noreply.github.com>
2025-03-24 22:36:53 -0300

    Aula 4

--- 04_operadores_logicos_e_condicoes/bissexto.py
+++ 04_operadores_logicos_e_condicoes/bissexto.py
@@ -1,22 +1,27 @@
 def eh_bissexto(ano):
-    # Preencher
-    pass
+    if ano / 4:
+        return True
 
 
 def proximo_bissexto(ano):
-    # Preencher
-    pass
+    if ano / 4:
+        return ano
+    else:
+        return int(ano + 3)
 
 
-assert eh_bissexto(0)
-assert eh_bissexto(2020)
-assert eh_bissexto(2024)
+def test():
+    assert eh_bissexto(0)
+    assert eh_bissexto(2020)
+    assert eh_bissexto(2024)
 
-assert not eh_bissexto(1)
-assert not eh_bissexto(2022)
-assert not eh_bissexto(2023)
+    assert not eh_bissexto(1)
+    assert not eh_bissexto(2022)
+    assert not eh_bissexto(2023)
 
-assert proximo_bissexto(2024) == 2024
-assert proximo_bissexto(2025) == 2028
-assert proximo_bissexto(2029) == 2032
-assert proximo_bissexto(2020) == 2020
+    assert proximo_bissexto(2024) == 2024
+    assert proximo_bissexto(2025) == 2028
+    assert proximo_bissexto(2029) == 2032
+    assert proximo_bissexto(2020) == 2020
+
+print(proximo_bissexto(2025))
