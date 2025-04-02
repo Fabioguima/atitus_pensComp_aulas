cf8b1e980284658a909e9df8f93d4cb403f3c450
Fábio Guimarães <37258543+Fabioguima@users.noreply.github.com>
2025-04-02 13:50:52 -0300

    Resolução exercicio extra 00 e 03 02/04

--- 07_Exercicios/extra_00.py
+++ 07_Exercicios/extra_00.py
@@ -1,18 +1,26 @@
-ANO_ATUAL = 2024
+ANO_ATUAL = 2025
 
 
 def saudacao(nome, sobrenome, ano_nascimento):
-    # Preencher
-    pass
 
+    # Calculando a idade do usuário
+    idade = ANO_ATUAL - ano_nascimento 
 
-assert (
+    if idade <= 0 or idade >= 150:
+        return None
+    else:
+        return print(f'Olá, {nome} {sobrenome}. Bom dia! Você possui {idade} anos!')
+
+def test():
+    assert (
     saudacao("Matheus", "Jardim", 1991)
     == "Olá, Matheus Jardim. Bom dia! Você possui 33 anos!"
-)
-assert (
+    )
+    assert (
     saudacao("Thais", "Silva", 1990)
     == "Olá, Thais Silva. Bom dia! Você possui 34 anos!"
-)
-assert saudacao("Matheus", "Jardim", 0) is None
-assert saudacao("Matheus", "Jardim", 2050) is None
+    )
+    assert saudacao("Matheus", "Jardim", 0) is None
+    assert saudacao("Matheus", "Jardim", 2050) is None
+
+print(saudacao("Matheus", "Jardim", 2050))
--- 07_Exercicios/extra_03.py
+++ 07_Exercicios/extra_03.py
@@ -1,8 +1,10 @@
 def real_para_dolar(valor, tx_conversao):
-    # Preencher
-    pass
+    #Calculo da cotação real --> Dolar
+    return print(f'{valor / tx_conversao} usd')
 
+def test():
+    assert real_para_dolar(500, 5.20) == 96.23
+    assert real_para_dolar(500, 1) == 500
+    assert real_para_dolar(500, 6) == 83.33333333333333
 
-assert real_para_dolar(500, 5.20) == 96.23
-assert real_para_dolar(500, 1) == 500
-assert real_para_dolar(500, 6) == 83.33333333333333
+print(real_para_dolar(500, 6))
