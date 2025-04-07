4ac7e99de6d53604fd347264e703dfe6cd829692
Fábio Guimarães <37258543+Fabioguima@users.noreply.github.com>
2025-04-07 20:54:55 -0300

    Corrigindo Erros

--- 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
+++ 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
@@ -11,13 +11,13 @@ def baskhara(a, b, c):
     elif discriminante > 0:
         x1 = (-b + (discriminante ** (1/2))) / (2 * a)
         x2 = (-b - (discriminante ** (1/2))) / (2 * a)
-        return [x1, x2]
+        return [x2, x1]
 
 def test():
-    #assert baskhara(1, -3, 2) == [2, 1]
-    #assert baskhara(2, 3, -2) == [-2, 0.5]
-    #assert baskhara(1, -5, 6) == [2, 3]
-    #assert baskhara(1, -7, 10) == [2, 5]
+    assert baskhara(1, -3, 2) == [2, 1]
+    assert baskhara(2, 3, -2) == [-2, 0.5]
+    assert baskhara(1, -5, 6) == [2, 3]
+    assert baskhara(1, -7, 10) == [2, 5]
 
     assert baskhara(1, 2, 3) is None
     assert baskhara(1, 0, 0) == 0
