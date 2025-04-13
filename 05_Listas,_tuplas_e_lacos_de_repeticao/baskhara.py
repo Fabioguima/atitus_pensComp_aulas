d7ee1b5b6129c0164bcc8ecd906c8da32fc611cf
Fábio Guimarães <37258543+Fabioguima@users.noreply.github.com>
2025-04-13 02:51:24 -0300

    Revendo automatização

--- 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
+++ 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
@@ -11,15 +11,15 @@ def baskhara(a, b, c):
     elif discriminante > 0:
         x1 = (-b + (discriminante ** (1/2))) / (2 * a)
         x2 = (-b - (discriminante ** (1/2))) / (2 * a)
-        return [x2, x1]
+        return [x1, x2]
 
 def test():
     assert baskhara(1, -3, 2) == [2, 1]
-    assert baskhara(2, 3, -2) == [-2, 0.5]
-    assert baskhara(1, -5, 6) == [2, 3]
-    assert baskhara(1, -7, 10) == [2, 5]
+    assert baskhara(2, 3, -2) == [0.5, -2.0]
+    assert baskhara(1, -5, 6) == [3.0, 2.0]
+    assert baskhara(1, -7, 10) == [5.0, 2.0]
 
     assert baskhara(1, 2, 3) is None
     assert baskhara(1, 0, 0) == 0
 
-print(baskhara(1, 0, 0) == 0)
+print(baskhara(1, -3, 2) == [2, 1])
