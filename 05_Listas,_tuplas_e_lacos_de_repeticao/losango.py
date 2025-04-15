1cba4aee0bb07a82316fb89307ef877439b7659d
Fábio Guimarães <37258543+Fabioguima@users.noreply.github.com>
2025-04-15 20:40:06 -0300

    Atualizando run_tests

--- 05_Listas,_tuplas_e_lacos_de_repeticao/losango.py
+++ 05_Listas,_tuplas_e_lacos_de_repeticao/losango.py
@@ -1,16 +1,20 @@
-altura = int(input("Digite um valor ímpar para a altura do losango: "))
-if altura % 2 == 0:
-    altura = altura + 1
-    print("O valor digitado era par, usaremos", altura, "no lugar")
+#altura = int(input("Digite um valor ímpar para a altura do losango: "))
 
-meio = altura // 2  
-for linha_atual in range(altura): 
-    if linha_atual <= meio:
-        num_espacos = meio - linha_atual
-        num_star = linha_atual * 2 + 1
-    else:  
-        num_espacos = linha_atual - meio
-        num_star = altura - (linha_atual - meio) * 2
-    print("." * num_espacos + "#" * num_star)
+def altura():
+    if altura % 2 == 0:
+        altura = altura + 1
+        print("O valor digitado era par, usaremos", altura, "no lugar")
 
+    meio = altura // 2  
+    for linha_atual in range(altura): 
+        if linha_atual <= meio:
+            num_espacos = meio - linha_atual
+            num_star = linha_atual * 2 + 1
+        else:  
+            num_espacos = linha_atual - meio
+            num_star = altura - (linha_atual - meio) * 2
+        print("." * num_espacos + "#" * num_star)
+
+def test():
+    assert altura(3)
 
