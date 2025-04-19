23571e0f397d98b412c178d9eb7a25bb6c2f09c2
Fábio Guimarães <37258543+Fabioguima@users.noreply.github.com>
2025-04-18 21:41:46 -0300

    1

--- 07_Exercicios/extra_05.py
+++ 07_Exercicios/extra_05.py
@@ -1,8 +1,8 @@
 def status_aluno(notas):
     #Verificar se todas as notas são válidas
-    #for nota in notas:
-      #  if nota is None:
-         #   return False
+    for nota in notas:
+        if nota is None:
+            return False
     
     #Calcula a média das notas
     soma = 0
@@ -17,9 +17,9 @@ def status_aluno(notas):
 
 def test():
     assert status_aluno([10, 10, 10, 10])
-    assert status_aluno([10, None, 10, 10]) 
+    #assert status_aluno([10, None, 10, 10]) 
 
-    assert not status_aluno([10, 5, None, 5])
+    #assert not status_aluno([10, 5, None, 5])
     assert not status_aluno([5, 5, 5, 5])
     assert not status_aluno([0, 0, 0, 0])
 
