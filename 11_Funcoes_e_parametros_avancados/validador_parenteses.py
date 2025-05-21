89d712d2b5911a4ae9d10e055c7deb0574098194
Fábio Guimarães <37258543+Fabioguima@users.noreply.github.com>
2025-05-20 23:59:55 -0300

    Ajusta validador_parenteses

--- 11_Funcoes_e_parametros_avancados/validador_parenteses.py
+++ 11_Funcoes_e_parametros_avancados/validador_parenteses.py
@@ -1,37 +1,17 @@
-def validador_parenteses(entrada: str) -> bool:
-    pass
-
-
-# Valores válidos
-assert validador_parenteses("()")
-assert validador_parenteses("()()")
-assert validador_parenteses("(())")
-assert validador_parenteses("(()()())")
-assert validador_parenteses("(((())()))")
-
-# Valores inválidos
-assert validador_parenteses(")")
-assert validador_parenteses("(")
-assert validador_parenteses("()(")
-assert validador_parenteses("()()())")
-assert validador_parenteses("(((())())")#Crie um método que recebe uma string S e retorna um bool indicando se a string é válida. Para ser válido:
+#Crie um método que recebe uma string S e retorna um bool indicando se a string é válida. Para ser válido:
 #Todo ‘(‘ precisa ser fechado com ‘)’. 
 #Não pode haver ‘)’ que não foi aberto.
 
 def validador_parenteses(entrada: str) -> bool:
-    parenteses_a = ''
-    parenteses_b = ''
-
+    parenteses = []
     for x in entrada:
         if x == '(':
-            parenteses_a += x
-        else:
-            parenteses_b += x
-
-    if len(parenteses_a) == len(parenteses_b):
-        return True
-    else:
-        return False
+            parenteses.append(x)
+        elif x == ')':
+            if not parenteses:
+                return False
+            parenteses.pop()
+    return len(parenteses) == 0
 
 
 def test():# Valores válidos
@@ -48,16 +28,15 @@ def test():# Valores inválidos
     assert validador_parenteses("()()())")
     assert validador_parenteses("(((())())")
 
-'''
+
 print(validador_parenteses("()"))
 print(validador_parenteses("()()"))
-print(validador_parenteses("(())"))    ------------> ASSERTS VÁLIDOS
+print(validador_parenteses("(())"))   # ------------> ASSERTS VÁLIDOS
 print(validador_parenteses("(()()())"))
 print(validador_parenteses("(((())()))"))
 
 print(validador_parenteses(")"))
 print(validador_parenteses("("))
-print(validador_parenteses("()("))     -------------> ASSERTS INVÁLIDOS
+print(validador_parenteses("()("))    # -------------> ASSERTS INVÁLIDOS
 print(validador_parenteses("()()())"))
 print(validador_parenteses("(((())())"))
-'''
